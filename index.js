b7f4068627235ad10ee806032ce5c670214a9b9a
barakatimothy <timothybaraka39@gmail.com>
2023-06-27 05:51:32 -0400

    complete

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ const len = word.length
+  word.toLowerCase()
+  for( let i=0;i<len/2;i++){
+    if(word[i]!== word[len -1 -i]){
+      return false 
+    }
+    return true
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+isPalindrome()
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
